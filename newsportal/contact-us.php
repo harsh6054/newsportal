3ea109ceaba7d6e75b0a449a1d207ea97539e743
Harshvardhan Patil <147519211+harsh6054@users.noreply.github.com>
2025-07-09 17:08:19 +0530

    Add files via upload

---
+++ newsportal/contact-us.php
@@ -0,0 +1,103 @@
+<?php
+include('includes/config.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>News Portal | Contact us</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <?php include('includes/header.php');?>
+    <!-- Page Content -->
+    <div class="container">
+
+<?php 
+$pagetype='contactus';
+$query=mysqli_query($con,"select PageTitle,Description from tblpages where PageName='$pagetype'");
+while($row=mysqli_fetch_array($query))
+{
+
+?>
+      <h1 class="mt-4 mb-3"><?php echo htmlentities($row['PageTitle'])?>
+  
+      </h1>
+
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <a href="index.php">Home</a>
+        </li>
+        <li class="breadcrumb-item active">Contact</li>
+      </ol>
+
+      
+      <div class="container">
+    <div class="row">
+        <div class="col-lg-8">
+            <!-- Main content goes here -->
+           
+               
+            </ol>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p><?php echo $row['Description'];?></p>
+                </div>
+            </div>
+            <!-- /.row -->
+        </div>
+        <div class="col-lg-4">
+            <!-- Feedback form goes here -->
+            <div class="feedback-form">
+                <h2>Feedback Form</h2>
+                <form action="https://formspree.io/f/mrgnylyn" method="post">
+                    <div class="form-group">
+                        <label for="name">Name:</label>
+                        <input type="text" id="name" name="name" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input type="email" id="email" name="email" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="message">Message:</label>
+                        <textarea id="message" name="message" required></textarea>
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php } ?>
+    
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+ <?php include('includes/footer.php');?>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
